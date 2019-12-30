cb0535994664392458bafafddf0afe948b09bf3f
HaoRui <hr854164641@qq.com>
2019-12-30 16:54:39 +0800

    更新

--- Graphix/loadModules/Graphix.cpp
+++ Graphix/loadModules/Graphix.cpp
@@ -17,7 +17,7 @@
 
 PYBIND11_MODULE(Graphix, m) {
 
-	m.def("version", []() {	return "version 0.0.1"; });
+	m.def("version", []() {	return "version 0.1.1"; });
 
 // --- Add bindings here ------------------------------------------------------------------
 
